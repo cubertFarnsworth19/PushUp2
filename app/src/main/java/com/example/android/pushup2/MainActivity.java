ac7de8a69f96e981a06a5e0670e46080b4a9e24c
cubertfarnsworth19 <d_cakebread@hotmail.com>
2017-05-24 19:06:32 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/pushup2/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.android.pushup2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TestUpload();
+    }
+
+
+    private void TestUpload(){
+        int i=1;
+        i++;
+    }
+}
